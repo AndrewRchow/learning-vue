984b7dacd0906e691ced31432aab2ee93294a491
Andrew Chow <chowlick@gmail.com>
2019-11-22 17:13:19 -0800

    array syntax done

--- 4-Class-and-Style-Bindings/index.js
+++ 4-Class-and-Style-Bindings/index.js
@@ -1,28 +1,37 @@
 var vm = new Vue({
-    el: '#example',
-    data: {
-        isActive: true,
-        hasError: false
-    },
+  el: '#example',
+  data: {
+    isActive: true,
+    hasError: false
+  },
 })
 
 
-var vm = new Vue({
-    el: '#example2',
-    data: {
-        isActive: true,
-        error: null
-      },
-      computed: {
-        classObject: function () {
-          return {
-            active: this.isActive && !this.error,
-            'text-danger': this.error && this.error.type === 'fatal',
-            test: true,
-            'test1':true
-          }
-        }
+var vm2 = new Vue({
+  el: '#example2',
+  data: {
+    isActive: true,
+    error: null
+  },
+  computed: {
+    classObject: function () {
+      return {
+        active: this.isActive && !this.error,
+        'text-danger': this.error && this.error.type === 'fatal',
+        test: true,
+        'test1': true
       }
+    }
+  }
+})
+
+var vm3 = new Vue({
+  el: '#example3',
+  data: {
+    activeClass: 'active',
+    errorClass: 'text-danger',
+    isTest:true
+  },
 })
 
 
