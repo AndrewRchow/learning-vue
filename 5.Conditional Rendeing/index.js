4c957434242cc769347e8a1372f949b8ef37e4cf
Andrew Chow <chowlick@gmail.com>
2019-12-11 17:17:28 -0800

    Conditional rendering done

--- 5.Conditional Rendeing/index.js
+++ 5.Conditional Rendeing/index.js
@@ -1,7 +1,29 @@
 var vm = new Vue({
     el: '#example',
     data: {
-        awesome: true,
+        awesome: 'AAA',
+        something: false,
     },
+  })
+  
+  var vm2 = new Vue({
+    el: '#example2',
+    data: {
+        type: 'B',
+    },
+  })
+  
+    
+  var vm3 = new Vue({
+    el: '#example3',
+    data: {
+        loginType: 'username',
+    },
+    methods: {
+        swap: function () {
+            this.loginType = this.loginType != 'username' ? 'username' : 'email';
+            console.log('test');
+        }
+    }
   })
   
